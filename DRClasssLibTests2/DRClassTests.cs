166ce71495160a8268118d252023a99970341801
Anders Hald <chald80@gmail.com>
2021-10-25 13:28:34 +0200

    Test Add

---
+++ DRClasssLibTests2/DRClassTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DRClasssLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRClasssLib.Tests
+{
+    [TestClass()]
+    public class DRClassTests
+    {
+        [TestMethod()]
+        public void DRClassTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DRClassTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
