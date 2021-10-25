33917740bdd73cdd8db945676ae0edc06c3da345
Anders Hald <chald80@gmail.com>
2021-10-25 13:04:47 +0200

    update class

--- DRClasssLib/DRClass.cs
+++ DRClasssLib/DRClass.cs
@@ -4,11 +4,80 @@ namespace DRClasssLib
 {
     public class DRClass
     {
-        
-        public int id { get; set; }
-        public string title { get; set; }
-        public string artist { get; set; }
-        public double duration { get; set; }
-        public int publicationYear { get; set; }
+        private int _id;
+        private string _title;
+        private string _artist;
+        private double _duration;
+        private int _publicationYear;
+
+        public DRClass(int id, string title, string artist, double duration, int publicationYear)
+        {
+            Id = id;
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            PublicationYear = publicationYear;
+        }
+        public DRClass()
+        {
+
+        }
+
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (value == null) throw new ArgumentNullException("Title", "Title is null");
+                if (value.Length < 2) throw new ArgumentException("Title length must be at least 2 characters");
+                _title = value;
+
+            }
+        }
+
+        public string Artist
+        {
+            get => _artist;
+            set
+            {
+                if (value == null) throw new ArgumentNullException("Artist", "Artist is null");
+                if (value.Length < 2) throw new ArgumentException("Artist length must be at least 2 characters");
+                _artist = value;
+
+            }
+        }
+        public double Duration 
+        { 
+            get => _duration;
+            set
+            {
+                if (value > 0) _duration = value;
+                else throw new ArgumentOutOfRangeException("Duration", value, "Illegal");
+            }
+
+        }
+
+        public int PublicationYear
+        {
+            get => _publicationYear;
+            set
+            {
+                if (value > 1900 && value < 2021) _publicationYear = value;
+                else throw new ArgumentOutOfRangeException("PublicationYear", value, "Illegal");
+            }
+        }
+
+
+
+
+
+
     }
 }
