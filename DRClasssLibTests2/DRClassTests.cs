3ed8640a9cb31fe1a80fd52dd668afd96a779fcf
Anders Hald <chald80@gmail.com>
2021-10-25 14:01:43 +0200

    UpdateTest

--- DRClasssLibTests2/DRClassTests.cs
+++ DRClasssLibTests2/DRClassTests.cs
@@ -24,8 +24,47 @@ namespace DRClasssLib.Tests
         {
             DRClass drC = new DRClass(1, "Bobob", "dadadad", 2323, 1999);
             Assert.AreEqual(1, drC.Id);
-            Assert.AreEqual("Bobob", drC.Artist);
+            Assert.AreEqual("Bobob", drC.Title);
+            Assert.AreEqual("dadadad", drC.Artist);
+            Assert.AreEqual(2323, drC.Duration);
+            Assert.AreEqual(1999, drC.PublicationYear);
+        }
+
+        [TestMethod]
+        public void TestDrClassTitle()
+        {
+            _drClass.Title = "Bobob";
+            Assert.AreEqual("Bobob", _drClass.Title);
+            Assert.ThrowsException<ArgumentNullException>(() => _drClass.Title = null);
+            Assert.ThrowsException<ArgumentException>(() => _drClass.Title = "a");
 
         }
+
+        public void TestDrClassArtist()
+        {
+            _drClass.Artist = "dadadad";
+            Assert.AreEqual("dadadad", _drClass.Artist);
+            Assert.ThrowsException<ArgumentNullException>(() => _drClass.Artist = null);
+            Assert.ThrowsException<ArgumentException>(() => _drClass.Artist = "a");
+
+        }
+
+        public void TestDrClassDuration()
+        {
+            _drClass.Duration = 100;
+            Assert.AreEqual(100, _drClass.Duration);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _drClass.Duration = 0);
+
+        }
+
+        public void TestDrClassPublicationYear()
+        {
+            _drClass.PublicationYear = 1999;
+            Assert.AreEqual(1999, _drClass.PublicationYear);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _drClass.PublicationYear = 2022);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _drClass.PublicationYear = 1899);
+
+        }
+
     }
 }
