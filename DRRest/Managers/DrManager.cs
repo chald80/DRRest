fbb2c5fa984d4fea8b0433c083fa40e4c704582f
Aleksander Kruse Schaumburg-Müller <alek1082@edu.easj.dk>
2021-10-26 23:07:40 +0200

    ss

--- DRRest/Managers/DrManager.cs
+++ DRRest/Managers/DrManager.cs
@@ -13,7 +13,7 @@ namespace DRRest.Managers
         private static readonly List<DRClass> Data = new List<DRClass>
         {
             new DRClass {UserId = 1, Id = _nextId++, Artist = "Duran Duran", Title = "Man eater",  Duration = 4.27, PublicationYear = 1986  },
-            new DRClass {UserId = 2, Id = _nextId++, Artist = "Kanye West", Title = "God",  Duration = 4.27, PublicationYear = 2021}
+            new DRClass {UserId = 1, Id = _nextId++, Artist = "Kanye West", Title = "God",  Duration = 4.27, PublicationYear = 2021}
             
         };
 
@@ -27,9 +27,9 @@ namespace DRRest.Managers
             return Data.FindAll(d => d.Title == title);
         }
 
-        public DRClass GetByUserId(int id)
+        public DRClass GetByUserId(int UserId)
         {
-            return Data.Find(d => d.UserId == id);
+            return Data.Find(d => d.UserId == UserId);
         }
 
 
